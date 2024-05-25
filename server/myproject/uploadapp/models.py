f30cfdd3eef9bad9f761737097857c4078c970e9
Mikita tsiutsiunou <mikita@qibus.com>
2024-05-25 18:02:15 +0300

    create image model

--- server/myproject/uploadapp/models.py
+++ server/myproject/uploadapp/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Image(models.Model):
+  CATEGORY_CHOICES = [
+    ('food', 'Food'),
+    ('nature', 'Nature'),
+    ('city', 'City')
+  ]
+
+  category = models.CharField(max_length=10, choices=CATEGORY_CHOICES)
+  image = models.ImageField(upload_to='images/')
+
+  def __str__(self):
+    return self.category
--- server/myproject/uploadapp/views.py
+++ server/myproject/uploadapp/views.py
@@ -1,3 +1,19 @@
+from django.views.decorators.csrf import csrf_exempt
 from django.shortcuts import render
+from django.http import HttpResponse
+from .forms import ImageUploadForm
+import os
 
-# Create your views here.
+# for avoid csrf token
+@csrf_exempt
+def upload_image(request):
+  if request.method == 'POST':
+    form = ImageUploadForm(request.POST, request.FILES)
+    if form.is_valid():
+      # save the form
+      form.save()
+    
+      category = form.cleaned_data['category']
+      image = form.cleaned_data['image']
+
+    return HttpResponse('done')
